bdc1a7d2147eb63f2e63caee329b24837a76099b
Kang Yiming <kangyiming2006@gmail.com>
2023-10-24 18:27:51 +0800

    initial commit

--- main.ts
+++ main.ts
@@ -1,104 +1,163 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TAbstractFile, Vault} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface RateMyDay {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+
+// this is an async function which takes in a string and returns a number, using the chatgpt api
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment-latest",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
 }
 
+
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+
 
 	async onload() {
-		await this.loadSettings();
+
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
-
+		
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-list-view',
+			name: 'Open List View',
 			callback: () => {
 				new SampleModal(this.app).open();
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'rate-current-note',
+			name: 'Rate current note',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				// get the content of the entire note
+				// get the current line
+				// get the current selection
+				
+				const files = this.app.vault.getMarkdownFiles();
+				// make a map of the filename to the file
+				const fileMap = new Map();
+				for (const file of files) {
+					fileMap.set(file.basename, file);
 				}
+				let text = view.getViewData();
+				query({ inputs: text }).then((result) => {
+					
+					let res = result[0][0].label;
+					let intensity = result[0][0].score;
+					let emoji = "";
+					// set the emoji based on the emotion as well as the intensity, using a 3 emojis for each emotion
+					if (res == "positive") {
+						if (intensity < 0.33) {
+							emoji = "😊";
+						} else if (intensity < 0.66) {
+							emoji = "😁";
+						} else {
+							emoji = "😄";
+						}
+					} else if (res == "neutral") {
+						if (intensity < 0.33) {
+							emoji = "😐";
+						} else if (intensity < 0.66) {
+							emoji = "😑";
+						} else {
+							emoji = "😶";
+						}
+					} else {
+						if (intensity < 0.33) {
+							emoji = "😞";
+						} else if (intensity < 0.66) {
+							emoji = "😔";
+						} else {
+							emoji = "😢";
+						}
+					}
+					// change the file name to append the emoji at the end
+					let filename = view.file?.basename;
+					let initial = filename;
+					// delete all  emojis in the filename
+					// check if the last character of the string is an emoji. if so, delete it.
+					const regex = /\p{Extended_Pictographic}/ug
+					filename = filename?.replace(regex, "");
+					
+					let newFilename = filename?.split(".")[0] + " " + emoji + ".md";
+					// get the file
+					let file = fileMap.get(initial);
+
+					// rename the file
+					this.app.vault.rename(file, newFilename);
+					// update the map
+					// save the file
+					
+				});
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
 class SampleModal extends Modal {
+	// make a modal that is a calendar
+
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		// make a Calendar in grid format
+		const files = this.app.vault.getMarkdownFiles();
+
+		let display = [];
+		const regex = /\p{Extended_Pictographic}/ug
+		for (const file of files) {
+			
+			let filename = file.basename;
+			// check if filename matches regex
+			if (regex.test(filename)) {
+				// if so add this file to the list of display files
+				display.push(file);
+			}
+		}
+		// sort the files by date
+		display.sort((a, b) => {
+			return a.stat.mtime - b.stat.mtime;
+		});
+		// display the files
+		let res = "";
+		for(const file of display) {
+			let filename = file.basename;
+			let date = file.stat.mtime;
+			let emoji = filename.match(regex);
+			filename = filename.replace(regex, "")
+			contentEl.createEl("h1", {text: filename + "-" + emoji});
+		}
+
 	}
 
 	onClose() {
@@ -106,11 +165,12 @@ class SampleModal extends Modal {
 		contentEl.empty();
 	}
 }
+/*
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: RateMyDay;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: RateMyDay) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -119,16 +179,6 @@ class SampleSettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
+*/
